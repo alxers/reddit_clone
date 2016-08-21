94766fcea378851e34a2f165417702a666ca5958
Alexey Ershov <alex4rom@gmail.com>
2016-08-21 13:05:10 +0300

    Modify layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+
+  def create
+    find_reddit
+    @post = @reddit.posts.create(posts_params)
+    redirect_to reddit_path(@reddit)
+  end
+
+  def new
+    find_reddit
+    @post = @reddit.posts.build
+  end
+
+  def show
+  end
+
+  private
+    def posts_params
+      params.require(:post).permit(:title, :body)
+    end
+
+    def find_reddit
+      @reddit = Reddit.find(params[:reddit_id])
+    end
 end
