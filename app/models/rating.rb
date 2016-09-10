69c5a68f73de8933100989bb41edfb982a86e193
Alexey Ershov <alex4rom@gmail.com>
2016-09-10 23:38:06 +0300

    Add whatever

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,4 +1,13 @@
 class Rating < ActiveRecord::Base
   belongs_to :posts
   belongs_to :comments
+
+  private
+    def up
+      self.update_attribute(votes: votes + 1)
+    end
+
+    def down
+      self.update_attribute(votes: votes - 1)
+    end
 end
