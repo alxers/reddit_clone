f33dfed2e2f3dc010d11fe6470f930659a9434aa
Alexey Ershov <alex4rom@gmail.com>
2016-09-13 17:26:30 +0300

    Add TODO

--- app/assets/javascripts/personal_messages.js
+++ app/assets/javascripts/personal_messages.js
@@ -1,6 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 
+// TODO: use onreadystatechange to check status of xhr request
 let xhttp = new XMLHttpRequest();
 
 document.addEventListener('click', function(e) {
