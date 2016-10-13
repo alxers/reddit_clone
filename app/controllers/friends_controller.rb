d29dc1159ac985311b16cad71cf9ca212895f198
Alexey Ershov <alex4rom@gmail.com>
2016-10-13 08:19:31 +0300

    Add few actions

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,10 @@
 class FriendsController < ApplicationController
+
+  def new
+    @friend = current_user.followers.build
+  end
+
+  def create
+    @friend = current_user.followers.new(friends_params)
+  end
 end
